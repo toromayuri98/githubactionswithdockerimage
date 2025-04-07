861a26412e32ccf512e03b88bef4906e807224b2
Mayuri <toromayuri98@gmail.com>
2025-04-07 11:04:36 +0530

    docker image with ECR setup

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def greeting():
-    return "Hello World!!"
+    return "Hello World from docker image!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
